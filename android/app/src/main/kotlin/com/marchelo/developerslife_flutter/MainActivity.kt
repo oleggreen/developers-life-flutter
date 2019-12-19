1b5f593e0b364722c4ecd854bfb117c41b1e7405
Oleg Green <oleg.green@innomos.com>
2019-12-19 15:50:39 +0200

    experiments

--- android/app/src/main/kotlin/com/marchelo/developerslife_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/marchelo/developerslife_flutter/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.marchelo.developerslife_flutter
 
+import android.content.Intent
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "test_activity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "startNewActivity") {
+                startNewActivity()
+            }
+        }
+    }
+
+    private fun startNewActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
 }
---
+++ android/app/src/main/kotlin/com/marchelo/developerslife_flutter/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.marchelo.developerslife_flutter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        title = "Native Activity"
+    }
+}
