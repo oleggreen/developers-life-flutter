5540825b4bab1993aa6803964a0b845fcc571190
Oleg Green <olegalex.green@gmail.com>
2020-03-20 01:47:43 +0200

    implementing updating category in model via proxy provider

--- lib/main_screen/view/home_screen.dart
+++ lib/main_screen/view/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:developerslife_flutter/data_provider.dart';
 import 'package:developerslife_flutter/main_screen/view/post_list.dart';
-import 'package:developerslife_flutter/main_screen/view_model/post_list_model.dart';
 import 'package:developerslife_flutter/main_screen/view/main_menu.dart';
 import 'package:developerslife_flutter/main_screen/view_model/selected_category_model.dart';
 import 'package:developerslife_flutter/model/categories.dart';
@@ -14,10 +13,6 @@ import 'package:developerslife_flutter/main.dart';
 import 'drawer_menu.dart';
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -32,24 +27,24 @@ class MyHomePage extends StatelessWidget {
         });
   }
 
-  Scaffold buildScaffold(BuildContext context, Category selectedCategory) {
-    var postListModel = Provider.of<PostListModel>(context, listen: false);
-    postListModel.loadCategory(selectedCategory);
+  Widget buildScaffold(BuildContext context, Category selectedCategory) {
+//    var postListModel = Provider.of<PostListModel>(context, listen: false);
+//    postListModel.loadCategory(selectedCategory);
 
     return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.dark,
-        title: Text(getTitleTextByCategory(selectedCategory, context), style: TextStyle(color: Colors.white)),
-        iconTheme: new IconThemeData(color: Colors.white),
-        actions: [createOverflowMenu()],
-      ),
-      drawer: MyDrawerWidget(),
-      body: Container(
-        color: lightGreyColor,
-        child: Center(
-          child: PostListWidget()
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          title: Text(getTitleTextByCategory(selectedCategory, context), style: TextStyle(color: Colors.white)),
+          iconTheme: new IconThemeData(color: Colors.white),
+          actions: [createOverflowMenu()],
         ),
-      ),
+        drawer: MyDrawerWidget(),
+        body: Container(
+          color: lightGreyColor,
+          child: Center(
+            child: PostListWidget()
+          ),
+        ),
     );
   }
 }
